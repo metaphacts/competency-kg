3f80a324b06d7f3707c29f3565469c6d399ee413
Nicolas Heist <nico@informatik.uni-mannheim.de>
2021-06-09 08:41:05 +0200

    Updated dashboard to work with local ScholKG data

--- import/import_data.sh
+++ import/import_data.sh
@@ -2,11 +2,13 @@
 
 printf "\n### %s ###\n\n" "Downloading CSO and ScholKG data"
 wget -q https://cso.kmi.open.ac.uk/download/version-3.2/CSO.3.2.ttl.zip
-unzip CSO.3.2.ttl.zip && rm CSO.3.2.ttl.zip
+unzip -q CSO.3.2.ttl.zip && rm CSO.3.2.ttl.zip
+echo "CSO data downloaded."
 
 wget -q https://scholkg.kmi.open.ac.uk/aikg/ontology -O scholkg-ontology.ttl
 wget -q https://scholkg.kmi.open.ac.uk/downloads/aikg.zip
 unzip -q aikg.zip && rm aikg.zip && mv aikg.ttl scholkg-data.ttl
+echo "ScholKG data downloaded."
 
 printf "\n### %s ###\n\n" "Uploading CS ontology into named graph http://cso.kmi.open.ac.uk/"
 curl -X POST --data-binary @import_cso.xml --header 'Content-Type:application/xml' http://localhost:8080/blazegraph/dataloader
