55cdcb3d3413e2692643950edfa03e2eb3ee9802
Nicolas Heist <nico@informatik.uni-mannheim.de>
2021-06-10 09:52:12 +0200

    Insert ontology statement and separate kmi ontology+data

--- import/import_data.sh
+++ import/import_data.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
 printf "\n### %s ###\n\n" "Downloading CSO and ScholKG data"
-wget -q https://cso.kmi.open.ac.uk/download/version-3.2/CSO.3.2.ttl.zip
+wget https://cso.kmi.open.ac.uk/download/version-3.2/CSO.3.2.ttl.zip
 unzip -q CSO.3.2.ttl.zip && rm CSO.3.2.ttl.zip
-echo "CSO data downloaded."
+# adding missing ontology statement to CSO ontology
+printf "\n<http://cso.kmi.open.ac.uk/schema/cso#> a owl:Ontology ; <http://www.linkedmodel.org/1.2/schema/vaem#namespace> <http://cso.kmi.open.ac.uk/schema/cso#> .\n" >> CSO.3.2.ttl
+echo "CSO data prepared."
 
-wget -q https://scholkg.kmi.open.ac.uk/aikg/ontology -O scholkg-ontology.ttl
-wget -q https://scholkg.kmi.open.ac.uk/downloads/aikg.zip
-unzip -q aikg.zip && rm aikg.zip && mv aikg.ttl scholkg-data.ttl
-echo "ScholKG data downloaded."
+wget https://scholkg.kmi.open.ac.uk/aikg/ontology -O kmi-ontology.ttl
+# adding missing ontology statement to KMI ontology
+printf "\n<http://scholkg.kmi.open.ac.uk/aikg/ontology#> a owl:Ontology ; <http://www.linkedmodel.org/1.2/schema/vaem#namespace> <http://scholkg.kmi.open.ac.uk/aikg/ontology#> .\n" >> kmi-ontology.ttl
+
+wget https://scholkg.kmi.open.ac.uk/downloads/aikg.zip
+unzip -q aikg.zip && rm aikg.zip && mv aikg.ttl kmi-data.ttl
+echo "KMI data prepared."
 
 printf "\n### %s ###\n\n" "Uploading CS ontology into named graph http://cso.kmi.open.ac.uk/"
 curl -X POST --data-binary @import_cso.xml --header 'Content-Type:application/xml' http://localhost:8080/blazegraph/dataloader
 
-printf "\n### %s ###\n\n" "Uploading ScholKG ontology and data into named graph http://scholkg.kmi.open.ac.uk/aikg/ (may take some minutes!)"
-curl -X POST --data-binary @import_scholkg.xml --header 'Content-Type:application/xml' http://localhost:8080/blazegraph/dataloader
+printf "\n### %s ###\n\n" "Uploading KMI ontology into named graph http://scholkg.kmi.open.ac.uk/aikg/ontology#"
+curl -X POST --data-binary @import_kmi-ont.xml --header 'Content-Type:application/xml' http://localhost:8080/blazegraph/dataloader
+
+printf "\n### %s ###\n\n" "Uploading KMI data into named graph http://scholkg.kmi.open.ac.uk/aikg/resource/ (may take some minutes!)"
+curl -X POST --data-binary @import_kmi-data.xml --header 'Content-Type:application/xml' http://localhost:8080/blazegraph/dataloader
 
 printf "\n### %s ###\n\n" "Data import successful"
